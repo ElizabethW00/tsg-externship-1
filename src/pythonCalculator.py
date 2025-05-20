c4a31576194ac2d28b4dccab3e92da6f90cc5cf2
Elizabeth Woo <71103650+ElizabethW00@users.noreply.github.com>
2025-05-20 14:53:35 -0400

    Update pythonCalculator.py

--- src/pythonCalculator.py
+++ src/pythonCalculator.py
@@ -11,14 +11,17 @@ def basicCalculator(operation, num1, num2):
     
     #if operation = ADD, return the sum
     if operation == Operation.ADD:
+        print("this workshop sucks")
         result = num1 + num2
     elif operation == Operation.SUBTRACT:
+        print("I hate working at this company")
         result = num1 - num2
     elif operation == Operation.MULTIPLY:
+        print("My boss is the worst")
         result = num1 * num2
     elif operation == Operation.DIVIDE:
         if num2 == 0:
-            print("Can't divide by 0")
+            print("u suck")
         else:
             result = num1 / num2
     return result
